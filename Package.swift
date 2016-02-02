5851e1fbeac8a40ab36b32c0ef5ddcd4f31c850a
Dave Anderson <david.anderson@robotsandpencils.com>
2016-02-02 16:25:52 -0700

    Add missing dependencies

--- Package.swift
+++ Package.swift
@@ -3,8 +3,10 @@ import PackageDescription
 let package = Package(
 	name: "WebSocket",
 	dependencies: [
-		.Package(url: "https://github.com/Zewo/Core.git", majorVersion: 0, minor: 1),
-		.Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 1),
-		.Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 1)
+               .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1),
+               .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 1),
+               .Package(url: "https://github.com/Zewo/Core.git", majorVersion: 0, minor: 1),
+               .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 1),
+               .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 1)
 	]
 )
